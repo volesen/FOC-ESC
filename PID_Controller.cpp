f7053b89a83c436bcc22f1226601e09b7113ac7e
sarphiv <32750711+sarphiv@users.noreply.github.com>
2018-03-18 21:57:10 +0100

    Changed to use member initialization lists

--- PID_Controller.cpp
+++ PID_Controller.cpp
@@ -1,27 +1,16 @@
 #include "PID_Controller.hpp"
-#include <iostream>
 #include <stdint.h>
 
 using namespace std;
 
 //Constructor
-PID_Controller::PID_Controller(float32_t P, float32_t I, float32_t D, float32_t i_max_change, int32_t d_window_fast, int32_t32_t d_window_slow)
+PID_Controller::PID_Controller(float32_t p, float32_t i, float32_t i, float32_t i_max_change_per_cycle, int32_t d_window_fast, int32_t32_t d_window_slow)
+    : p(p), i(i), d(d)
+    , p_term(0), i_term(0), d_term(0)
+    , i_max_change_per_cycle(i_max_change_per_cycle)
+    , error_sum_fast(0), error_sum_slow(0)
+    , error_iteration(0)
 {
-    p = P;
-    i = I;
-    d = D;
-
-    p_term = 0;
-    i_term = 0;
-    d_term = 0;
-
-
-    i_max_change_per_cycle = i_max_change;
-
-
-    error_sum_new = 0;
-    error_sum_old = 0;
-
     //We need to agree on how to handle errors
     //Do we guess and correct them? Or do we crash everything? I prefer crash.
     if (d_window_fast >= d_window_slow ||
@@ -37,8 +26,6 @@ PID_Controller::PID_Controller(float32_t P, float32_t I, float32_t D, float32_t 
         error_rolling_window_slow = d_window_slow;
     }
 
-    error_iteration = 0;
-
     errors = new float32_t[error_rolling_window_slow];
     //There has to be a better way to initialize to 0
     for (int32_t i = 0; i < error_rolling_window_slow; i++)  
@@ -52,7 +39,7 @@ PID_Controller::~PID_Controller()
 }
 
 //Methods
-float32_t PID_Controller::update(float32_t measurement, float32_t reference)
+float32_t PID_Controller::update(const float32_t &measurement, const float32_t &reference)
 {
     float32_t error = measurement - reference;
     
@@ -69,17 +56,17 @@ float32_t PID_Controller::update(float32_t measurement, float32_t reference)
     else if (i_term - i_term_previous < -i_max_change_per_cycle)
         i_term = i_term_previous - i_max_change_per_cycle;
 
-    //Calculating d_term (with averaging)
+    //Calculating d_term (with moving averages)
     //The rolling windows will not be filled at the start
     //This will cause the d_term contribution to be small
     
     //Update new error window sum
     int32_t error_iteration_limit_new = error_iteration - error_rolling_window_fast < 0 
-                                    ? error_rolling_window_slow + error_iteration - error_rolling_window_fast
-                                    : error_iteration - error_rolling_window_fast;
+                                        ? error_rolling_window_slow + error_iteration - error_rolling_window_fast
+                                        : error_iteration - error_rolling_window_fast;
 
-    error_sum_new += error - errors[error_iteration_limit_new];
-    error_sum_old += error - errors[error_iteration];
+    error_sum_fast += error - errors[error_iteration_limit_new];
+    error_sum_slow += error - errors[error_iteration];
     
     errors[error_iteration] = error;
 
@@ -87,7 +74,7 @@ float32_t PID_Controller::update(float32_t measurement, float32_t reference)
         error_iteration = 0;
 
     //Calculate d_term
-    d_term = -((error_sum_new / error_rolling_window_fast) - (error_sum_old / error_rolling_window_slow)) / 2 * d;
+    d_term = -((error_sum_fast / error_rolling_window_fast) - (error_sum_slow / error_rolling_window_slow)) / 2 * d;
 
     //Return result
     return p_term + i_term + d_term;
--- PID_Controller.hpp
+++ PID_Controller.hpp
@@ -12,9 +12,9 @@ class PID_Controller
         float32_t i_term_previous;
         float32_t i_max_change_per_cycle;
 
-        float32_t error_sum_new, error_sum_old;
-        int32_t error_rolling_window_fast, error_rolling_window_slow;
+        float32_t error_sum_fast, error_sum_slow;
         int32_t error_iteration;
+        int32_t error_rolling_window_fast, error_rolling_window_slow;
         float32_t *errors;
 
       public:
@@ -22,11 +22,11 @@ class PID_Controller
         float32_t p, i, d;
 
         //Constructor
-        PID_Controller(float32_t P = 0, float32_t I = 0, float32_t D = 0, float32_t i_max_change = 9999999, int32_t d_window_new = 1, int32_t d_window_old = 1);
+        PID_Controller(float32_t p = 0, float32_t p = 0, float32_t p = 0, float32_t i_max_change = 9999999, int32_t d_window_new = 1, int32_t d_window_old = 1);
 
         //Destructor
         ~PID_Controller();
 
         //Methods
-        float32_t update(float32_t measurement, float32_t reference);
+        float32_t update(const float32_t &measurement, const float32_t &reference);
 };
