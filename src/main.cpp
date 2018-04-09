d9310a4458043c956a3b8428809ded1a0a086ae3
sarphiv <32750711+sarphiv@users.noreply.github.com>
2018-04-09 08:21:28 +0200

    In progress: Testing PID and Transform

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,49 @@
 #include <Arduino.h>
 
 #include "Transform.hpp"
+#include "PID.hpp"
 
 uint16_t angle = 0;
+
+Idq dq;
+Iabc abc;
+
+PID_Controller PID_waste(0.9, 0.8, 0, 0.7);
+PID_Controller PID_torque(0.9, 0.8, 0, 0.7);
 
 void setup()
 {
     Serial.begin(9600);
+
+    abc.Ia = 0;
+    abc.Ib = 0;
+    abc.Ic = 0;
 }
 
 void loop() 
 {
-    Idq dq;
+    dq = Transform::de_phase(angle, abc);
 
-    dq.Id = 9;
-    dq.Iq = 569;
+    dq.Id = PID_waste.update(dq.Id, (float)0);
+    dq.Iq = PID_torque.update(dq.Iq, (float)50);
 
-    Iabc abc = Transform::to_phase(angle, dq);
+    abc = Transform::to_phase(angle, dq);
+
+    abc.Ia = abc.Ia +3;
+    abc.Ib = abc.Ib -4;
+    abc.Ic = abc.Ic +2;
 
     Serial.print(abc.Ia);
     Serial.print(',');
     Serial.print(abc.Ib);
     Serial.print(',');
-    Serial.println(abc.Ic);
+    Serial.print(abc.Ic);
+    Serial.print(',');
 
-    dq = Transform::de_phase(angle, abc);
+    Serial.print(dq.Id);
+    Serial.print(',');
+    Serial.println(dq.Iq);
 
-    // Serial.print(dq.Iq);
-    // Serial.print(',');
-    // Serial.println(dq.Id);
 
     angle < MECHANICAL_ROTATION_STEPS
         ? angle++
