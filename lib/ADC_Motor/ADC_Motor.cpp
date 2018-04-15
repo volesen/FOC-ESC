175e5cad83c9ee15b49f1f656bbbcd4628e9246f
sarphiv <32750711+sarphiv@users.noreply.github.com>
2018-04-15 14:20:47 +0200

    Made ADC_Motor use motor_id better

--- lib/ADC_Motor/ADC_Motor.cpp
+++ lib/ADC_Motor/ADC_Motor.cpp
@@ -1,8 +1,8 @@
 #include "ADC_Motor.hpp"
 
 ADC_Motor::ADC_Motor(motor_id motor)
-    : phase_A(motor == motor_id::motor0 ? ADC_PIN::M0_PHASE_A : ADC_PIN::M1_PHASE_A),
-      phase_B(motor == motor_id::motor0 ? ADC_PIN::M0_PHASE_B : ADC_PIN::M1_PHASE_B)
+    : phase_A(motor == motor0 ? ADC_PIN::M0_PHASE_A : ADC_PIN::M1_PHASE_A),
+      phase_B(motor == motor0 ? ADC_PIN::M0_PHASE_B : ADC_PIN::M1_PHASE_B)
 {
 
 }
